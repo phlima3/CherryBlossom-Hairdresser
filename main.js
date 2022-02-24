1db98710f3be4504d3e53c5cce7c00fd0389e2db
PEDRO HENRIQUE DOS SANTOS LIMA <psantoslima86@gmail.com>
2022-02-24 11:04:05 -0300

    pedro
    Adicionei função aos botões do menu para fechar o menu

--- main.js
+++ main.js
@@ -6,5 +6,13 @@ const toggle =
 for (const element of toggle) {
   element.addEventListener('click', function () {
     nav.classList.toggle('show')
+  }) /* Adiciona uma função aos botões de menu para abrir e fechar menu */
+}
+
+/*Quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
   })
 } /* Adiciona uma função aos botões de menu para abrir e fechar menu */
