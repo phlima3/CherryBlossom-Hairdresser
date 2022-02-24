7e0c888da7c7e5f9e134020c7c47f82cd73cf0ea
PEDRO HENRIQUE DOS SANTOS LIMA <psantoslima86@gmail.com>
2022-02-24 10:45:11 -0300

    pedro
    Adicionei a função de abrir e fechar o menu ao clicar nos ícones

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+/* Abre e fecha menu quando clica no incone */
 const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('.nav toggle')
+const toggle =
+  document.querySelectorAll('nav .toggle') /*Declarei as constantes */
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+} /* Adiciona uma função aos botões de menu para abrir e fechar menu */
