58d5c70a0c1449aef81b6cac0cae660e81d036ad
PEDRO HENRIQUE DOS SANTOS LIMA <psantoslima86@gmail.com>
2022-02-28 12:48:49 -0300

    pedro
    finalização do projeto em mobile

--- main.js
+++ main.js
@@ -18,10 +18,11 @@ for (const link of links) {
 }
 
 /* mudar o header da página quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+
   if (window.scrollY >= navHeight) {
     // scroll é Maior que a altura do header
     header.classList.add('scroll')
@@ -29,7 +30,7 @@ window.addEventListener('scroll', function () {
     //Menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testmonial carousel slider swiper*/
 
@@ -40,4 +41,40 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Quando rolar a página */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
 })
